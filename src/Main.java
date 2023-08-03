ac911cc65dd874afdb431bdf0a4020337f633c54
GoldenEagle75 <marshall.muhlhauser@gmail.com>
2023-08-03 17:14:24 -0400

    File Working

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
 public class Main {
     public static void main(String[] args) {
-        
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile = null;
+        String rec;
+        int line = 0;
+        int words = 0;
+        int characters = 0;
+        String fileName;
+
+        try {
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                while (reader.ready()){
+                    rec = reader.readLine();
+                    line ++;
+                    characters = characters + rec.length();
+                    words = words + rec.split(" ", 0).length;
+                }
+                reader.close();
+                System.out.println("\n\nData file read!");
+            }
+        }
+
+        catch (FileNotFoundException e){
+            System.out.println("File not found!");
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        fileName = selectedFile.getName();
+
+        System.out.printf("Your file, %s, has %s lines, %s words, and %s characters.", fileName, line, words, characters);
     }
 }
